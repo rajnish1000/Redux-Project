da650226296eab0f1a5cc23253d5859f06526c8d
rajnish2912 <rajnishsharma29120@gmail.com>
2023-03-11 13:44:54 +0530

    redux

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+import userRouter from "./routes/user.js";
+import tourRouter from "./routes/tour.js";
+// import dotenv from "dotenv";
+
+const app = express();
+// dotenv.config();
+
+app.use(morgan("dev"));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/users", userRouter); // http://localhost:5000/users/signup
+app.use("/tour", tourRouter);
+app.get("/", (req, res) => {
+  res.send("Welcome to tour API");
+});
+
+ const MONGODB_URL="mongodb+srv://rajnishmig51:93tjP3TuSVL2RTWi@cluster0.z36vnlf.mongodb.net/?retryWrites=true&w=majority"
+ const port = 5000;
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
